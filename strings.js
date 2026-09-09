64c18365a4078dd83d8722173257fa1015e21b41
saucebadger <260590675+saucebadger@users.noreply.github.com.>
2026-09-08 19:56:31 -0600

    add js fuction practice

---
+++ strings.js
@@ -0,0 +1,9 @@
+const firstName = "Gil";
+const item = "Dragonite Plushie";
+const store = "PixelForge Collectibles";
+
+let message = "Hello " + firstName + "! " + "Your " + item + " order from " + store + " is ready!";
+console.log(message);
+
+let message2 = `Lets see if ${firstName}! actually knows how to order from ${store} !`;
+console.log(message2)
